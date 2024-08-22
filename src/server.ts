22d5d50d7d5725aa7e6cad6f754ebf258d819b03
Alessandro barbosa <66150051+Alessandro-barbosa@users.noreply.github.com>
2024-08-22 08:42:39 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,18 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("Olá pessoal, tudo bem? Quero fazer mais uma mudança");
-});
-
 //Inserir usuários
 app.post("/user", async function (req: Request, res: Response) {
   //Inserir um usuário ...
   try {
     const userdata = req.body;
-    console.log("Requisição enviada pelo método posto: ");
+    if(!userdata.email){
+      return res.json({
+        status: 400,
+        message: "sem email"
+    })
+    }    
+    console.log("Requisição enviada pelo método post: ");
     console.log(userdata);
     const newuser = await prisma.user.create({
       data: userdata,
@@ -44,15 +46,20 @@ app.get("/users", async function (req: Request, res: Response) {
   res.json(users);
 });
 
+app.get("/users-data", async function (req: Request, res: Response) {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
 // Atualizar usuários
 
-app.put("/user", async function (req: Request, res: Response) {
+app.put("/user/:id", async function (req: Request, res: Response) {
   const userData = req.body;
-  console.log(userData["email"])
+  const userId = req.params.id;
   try{
     const updateUser = await prisma.user.update({
       where: {
-        email: userData["email"]
+        id: parseInt(userId),
       },
       data: userData
     })
@@ -70,12 +77,14 @@ app.put("/user", async function (req: Request, res: Response) {
 
 // Deletar usuários
 
-app.delete("/user", async function (req: Request, res: Response) {
+app.delete("/user/:id", async function (req: Request, res: Response) {
   const userData = req.body;
+  const userId = req.params.id;
+
   try{
     const deleteUser = await prisma.user.delete({
       where:{
-        email: userData["email"]
+        id: parseInt(userId)
       }
     })
     res.json({
@@ -88,6 +97,35 @@ app.delete("/user", async function (req: Request, res: Response) {
       status: 500,
       message: error
     })
+  }
+});
+
+// Listar posts
+app.get("/posts", async function (req: Request, res: Response) {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
+});
+
+// Criar um post para um usuário
+app.post("/post", async function (req: Request, res: Response){
+  const postData = req.body;
+  try{
+      const newPost = await prisma.post.create({
+          data: postData,
+      });
+      console.log(postData);
+      console.log("Post feito com sucesso");
+      res.json({
+        status: 200,
+        newPost: newPost
+      })
+  }catch(error){
+      console.log(postData);
+      res.json({
+          status: 500,
+          message: error,
+      })
+      console.log(error);
   }
 });
 
